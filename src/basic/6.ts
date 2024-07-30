37be91e3c6b48c243c451943fdd093e95cb6761b
Spinkll <roma.skorohod.2005@gmail.com>
2024-07-30 15:44:34 +0300

    fix

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -7,3 +7,19 @@ interface User {
     country: string;
   };
 }
+
+const mango: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
